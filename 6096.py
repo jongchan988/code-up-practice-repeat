5fc8d9e2c54513f28a17a27b8d8fb1bb2e683b14
jongchan988@gmail.com <jongchan988@gmail.com>
2023-09-02 11:42:50 +0900

    삭제: 6096 잘못된 주석

--- 6096.py
+++ 6096.py
@@ -1,13 +1,10 @@
 panSize = 19
-
-
 pan = []
 
 for i in range(panSize):
     inputArr = input().split()
     pan.append(inputArr)
 
-# 십자뒤집기 횟수
 n = int(input())
 
 for i in range(n):
@@ -15,7 +12,6 @@ for i in range(n):
     x = int(x) -1
     y = int(y) -1
 
-    # 가로 뒤집기
     for j in range(panSize):
         pan[x][j] = 1 if(pan[x][j] == 0) else 0
         pan[j][y] = 1 if(pan[j][y] == 0) else 0
