384e6d43a7fea49b4f3cc6fd7f6a4404e2ffa4a3
jongchan988@gmail.com <jongchan988@gmail.com>
2023-09-02 13:03:18 +0900

    refactor: 6096.py

--- 6096.py
+++ 6096.py
@@ -1,21 +1,23 @@
 panSize = 19
-pan = []
+pan = [[0] * panSize for _ in range(panSize)]
 
 for i in range(panSize):
     inputArr = input().split()
-    pan.append(inputArr)
+    for j in range(panSize):
+        pan[i][j] = int(inputArr[j])
 
 n = int(input())
 
 for i in range(n):
     x, y = input().split()
-    x = int(x) -1
-    y = int(y) -1
+    x = int(x) - 1
+    y = int(y) - 1
 
     for j in range(panSize):
-        pan[x][j] = 1 if(pan[x][j] == 0) else 0
-        pan[j][y] = 1 if(pan[j][y] == 0) else 0
+        pan[x][j] = 0 if pan[x][j] == 1 else 1
+        pan[j][y] = 0 if pan[j][y] == 1 else 1
 
+# 바둑판 상태 출력
 for i in range(panSize):
     for j in range(panSize):
         print(pan[i][j], end=" ")
